2f6979d6d7a7ba348c107d298310f1da7a8e3355
kaixugege <604312074@qq.com>
2018-11-02 11:55:02 +0800

    for test

---
+++ app/src/main/java/com/kaixugege/kaixu/mvp/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.kaixugege.kaixu.mvp;
+
+/**
+ * @Author: KaixuGege
+ * Time:           2018/11/2
+ * ProjectName:    KaixugegeMVP
+ * ClassName:
+ * Info:
+ */
+public class BaseActivity {
+}
